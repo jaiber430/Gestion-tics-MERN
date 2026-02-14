b6fb89adafe46aeed10c6574356b4efc7ef4dffa
jaiber430 <jaiber4302@gmail.com>
2026-02-14 17:15:51 -0500

    Limpieza de controller solicitud/Regular

--- backend/src/controllers/solicitudController.js
+++ backend/src/controllers/solicitudController.js
@@ -4,6 +4,8 @@ import solicitudCerradaService from "../services/solicitudCerradaServices.js"
 import solicitudAbiertaService from "../services/solicitudAbiertaservices.js"
 import ProgramassEspecialesCampesena from "../models/ProgramassEspecialesCampesena.js"
 
+import HttpErrors from "../helpers/httpErrors.js"
+
 const tipoSolicitud = async (req, res) => {
     const { solicitud } = req.params
 
@@ -59,75 +61,13 @@ const crearSolicitud = async (req, res) => {
         // Todas las operaciones correctas que usen el la sesion van a ser creadas
         const session = await mongoose.startSession()
 
+        const tipoSolicitud = 'Regular'
         try {
             session.startTransaction()
-            // Tipo de solicitud en base al id de la url
-            const tipoSolicitud = 'Regular'
             // Pedir campos en base a la oferta
-            if (tipoOferta !== "Abierta") {
-                // VALIDAR TODOS LOS CAMPOS INCLUYENDO LOS DEL HORARIO QUE TÚ ENVÍAS
-                if (!tipoOferta || !cupo || !direccionFormacion || !subSectorEconomico || !convenio || !ambiente || !nombreEmpresa || !nombreResponsable || !emailEmpresa || !nitEmpresa || !tipoEmpresa || !cartaSolicitud || !programaFormacion || !programaEspecial || !municipio || !fechaInicio || !horaInicio || !horaFin || !fechasSeleccionadas) {
-                    throw new HttpErrors('Todos los campos son requeridos', 400)
-                }
+            if (tipoOferta === "Abierta") {
 
-                // Verificaciones adicionales para la creación
-                const { existeTipoEmpresa } = await empresaValidator(req.body, session)
-
-                const crearEmpresa = await Empresa.create([{
-                    nombreEmpresa: nombreEmpresa,
-                    nombreResponsable: nombreResponsable,
-                    emailEmpresa: emailEmpresa,
-                    nitEmpresa: nitEmpresa,
-                    tipoEmpresa: existeTipoEmpresa._id,
-                    cartaSolicitud: cartaSolicitud
-                }], { session })
-
-                // Verificaciones y Creación del horario
-                const { programaExiste, existeProgramaEspecial, existeMunicipio } = await solicitudValidator(req.body, session)
-
-                const horario = construirHorario(programaExiste, {
-                    fechasSeleccionadas: req.body.fechasSeleccionadas,
-                    horaInicio: req.body.horaInicio,
-                    fechaInicio: req.body.fechaInicio,
-                    horaFin: req.body.horaFin
-                });
-
-                const evitarTranslape = await Solicitud.findOne({
-                    direccionFormacion: req.body.direccionFormacion,
-                    ambiente: req.body.ambiente,
-                    municipio: existeMunicipio._id,
-                    fechaInicio: horario.fechaInicio,
-                    fechaFin: horario.fechaFin,
-                    horaInicio: horario.horaInicio,
-                    horaFin: horario.horaFin,
-                    fechasSeleccionadas: horario.fechasSeleccionadas
-                }).session(session)
-
-                if (evitarTranslape) {
-                    throw new HttpErrors('Ya se ha creado una solicitud en el horario y dirección', 403)
-                }
-
-                const nuevaSolicitud = await Solicitud.create([{
-                    tipoSolicitud: tipoSolicitud,
-                    tipoOferta: tipoOferta,
-                    cupo: cupo,
-                    direccionFormacion: direccionFormacion,
-                    subSectorEconomico: subSectorEconomico,
-                    convenio: convenio,
-                    ambiente: ambiente,
-                    usuarioSolicitante: req.usuario.id,
-                    empresaSolicitante: crearEmpresa[0]._id,
-                    municipio: existeMunicipio._id,
-                    programaFormacion: programaExiste._id,
-                    programaEspecial: existeProgramaEspecial._id,
-                    fechaInicio: horario.fechaInicio,
-                    fechaFin: horario.fechaFin,
-                    mes1: horario.mes1,
-                    mes2: horario.mes2,
-                    horaInicio: horario.horaInicio,
-                    horaFin: horario.horaFin,
-                    fechasSeleccionadas: horario.fechasSeleccionadas
-                }], { session });
+                const { nuevaSolicitud } = await solicitudAbiertaService(req.body, session, tipoOferta, req.usuario.id, tipoSolicitud)
 
                 await session.commitTransaction()
                 session.endSession()
@@ -137,56 +77,8 @@ const crearSolicitud = async (req, res) => {
                     solicitud: nuevaSolicitud[0]
                 })
             } else {
-                if (!tipoOferta || !cupo || !direccionFormacion || !subSectorEconomico || !convenio || !ambiente || !programaFormacion || !programaEspecial || !municipio || !fechaInicio || !horaInicio || !fechasSeleccionadas) {
-                    throw new HttpErrors('Todos los campos son requeridos', 400)
-                }
 
-                // Verificaciones y Creación del horario
-                const { programaExiste, existeProgramaEspecial, existeMunicipio } = await solicitudValidator(req.body, session)
-
-                const horario = construirHorario(programaExiste, {
-                    fechasSeleccionadas: req.body.fechasSeleccionadas,
-                    horaInicio: req.body.horaInicio,
-                    fechaInicio: req.body.fechaInicio,
-                    horaFin: req.body.horaFin
-                });
-
-                const evitarTranslape = await Solicitud.findOne({
-                    direccionFormacion: req.body.direccionFormacion,
-                    ambiente: req.body.ambiente,
-                    municipio: existeMunicipio._id,
-                    fechaInicio: horario.fechaInicio,
-                    fechaFin: horario.fechaFin,
-                    horaInicio: horario.horaInicio,
-                    horaFin: horario.horaFin,
-                    fechasSeleccionadas: horario.fechasSeleccionadas
-                }).session(session)
-
-                if (evitarTranslape) {
-                    throw new HttpErrors('Ya se ha creado una solicitud en el horario y dirección', 403)
-                }
-
-                const nuevaSolicitud = await Solicitud.create([{
-                    tipoSolicitud: tipoSolicitud,
-                    tipoOferta: tipoOferta,
-                    cupo: cupo,
-                    direccionFormacion: direccionFormacion,
-                    subSectorEconomico: subSectorEconomico,
-                    convenio: convenio,
-                    ambiente: ambiente,
-                    usuarioSolicitante: req.usuario.id,
-                    empresaSolicitante: null,
-                    municipio: existeMunicipio._id,
-                    programaFormacion: programaExiste._id,
-                    programaEspecial: existeProgramaEspecial._id,
-                    fechaInicio: horario.fechaInicio,
-                    fechaFin: horario.fechaFin,
-                    mes1: horario.mes1,
-                    mes2: horario.mes2,
-                    horaInicio: horario.horaInicio,
-                    horaFin: horario.horaFin,
-                    fechasSeleccionadas: horario.fechasSeleccionadas
-                }], { session });
+                const {nuevaSolicitud} = await solicitudCerradaService(req.body, session, tipoOferta, req.usuario.id, tipoSolicitud)
 
                 await session.commitTransaction()
                 session.endSession()
