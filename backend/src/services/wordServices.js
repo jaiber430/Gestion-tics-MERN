ebbe665cde0a88b8340d153c6326a307206d870d
jaiber430 <jaiber4302@gmail.com>
2026-02-15 00:39:02 -0500

    Toca formatear las fechas + colocar
    Programas Especiales
    Dias de semana

--- backend/src/services/wordServices.js
+++ backend/src/services/wordServices.js
@@ -4,16 +4,25 @@ import fs from "fs"
 import path from "path"
 import PizZip from "pizzip"
 import Docxtemplater from "docxtemplater"
-import Solicitud from '../models/Solicitud.js'
 import Usuarios from "../models/Usuarios.js"
+import ProgramasFormacion from "../models/ProgramasFormacion.js"
+import Municipios from "../models/Municipios.js"
+import Empresa from "../models/Empresa.js"
 
 export const generarDocumento = async (data) => {
+    // Crear la ruta donde se encuntra la plantilla
     const rutaFichaCaracterizacion = path.join(process.cwd(), 'src', 'public', 'plantilla.docx')
+    // Leer el archivo encontrado
     const content = fs.readFileSync(rutaFichaCaracterizacion, "binary")
 
-    const { usuarioSolicitante } = data
+    let empresaCreada = ''
 
+    const { usuarioSolicitante, programaFormacion, fechaInicio, fechaFin, cupo, municipio, direccionFormacion, empresaSolicitante, subSectorEconomico, convenio, ambiente, horaInicio, horaFin, mes1, mes2 } = data
+
+    const dataProgramaFormacion = await ProgramasFormacion.findById(programaFormacion)
     const dataUser = await Usuarios.findById(usuarioSolicitante)
+    const dataMunicipio = await Municipios.findById(municipio)
+    const dataEmpresa = await Empresa.findById(empresaSolicitante)
 
     const zip = new PizZip(content)
 
@@ -23,10 +32,27 @@ export const generarDocumento = async (data) => {
     })
 
     doc.render({
+        codigoPrograma: dataProgramaFormacion.codigoPrograma,
+        nombrePrograma: dataProgramaFormacion.nombrePrograma,
+        versionPrograma: dataProgramaFormacion.versionPrograma,
+        horas: dataProgramaFormacion.horas,
+        fechaInicio: fechaInicio,
+        fechaFin: fechaFin,
+        cupo: cupo,
+        municipio: dataMunicipio.municipios,
+        direccionFormacion: direccionFormacion,
         nombre: dataUser.nombre,
         apellido: dataUser.apellido,
         numeroDoc: dataUser.numeroIdentificacion,
-        email: dataUser.email
+        email: dataUser.email,
+        nombreEmpresa: dataEmpresa ? dataEmpresa.nombreEmpresa : '',
+        subSectorEconomico: subSectorEconomico,
+        convenio: convenio,
+        ambiente: ambiente,
+        horaFin: horaFin,
+        horaInicio: horaInicio,
+        mes1: mes1,
+        mes2: mes2,
     })
 
     const buffer = doc.getZip().generate({
