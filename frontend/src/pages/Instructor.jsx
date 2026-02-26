3777400f55c608bd1e9fc73e4ae45b600c4a1f33
jaiber430 <jaiber4302@gmail.com>
2026-02-26 09:41:13 -0500

    Eliminando cosas inecesarias

--- frontend/src/pages/Instructor.jsx
+++ frontend/src/pages/Instructor.jsx
@@ -10,12 +10,6 @@ const Instructor = () => {
     const mostrarCards = location.pathname === '/instructor'
 
     const menuItems = [
-        // { 
-        //     id: 'perfil',
-        //     label: 'Perfil', 
-        //     icon: 'M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z',
-        //     color: 'text-blue-500'
-        // },
         { 
             id: 'campesena',
             label: 'CampeSENA', 
